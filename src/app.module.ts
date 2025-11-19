e7c1ae493edbc1b499c7f77d13d1f4bd168f4098
Jose Carlos <jose890823@gmail.com>
2025-11-18 20:00:45 -0600

    Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,11 @@ import { HealthModule } from './modules/health/health.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_DATABASE || 'screenshoot_fx',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: process.env.NODE_ENV !== 'production', // Auto-sync en desarrollo
       logging: process.env.NODE_ENV === 'development',
@@ -35,3 +35,12 @@ import { HealthModule } from './modules/health/health.module';
   providers: [AppService],
 })
 export class AppModule {}
+
+// host: process.env.DB_HOST || 'localhost',
+//       port: parseInt(process.env.DB_PORT || '5432', 10),
+//       username: process.env.DB_USERNAME || 'postgres',
+//       password: process.env.DB_PASSWORD || 'postgres',
+//       database: process.env.DB_DATABASE || 'screenshoot_fx',
+//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//       synchronize: process.env.NODE_ENV !== 'production', // Auto-sync en desarrollo
+//       logging: process.env.NODE_ENV === 'development',
